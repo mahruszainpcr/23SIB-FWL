4ece19baf78b735cae38b8007d5a8e13c0c7da23
Indi Marsela <indi23si@mahasiswa.pcr.ac.id>
2025-05-14 15:57:49 +0700

    indi commit

--- src/pages/latihangit/Page18.jsx
+++ src/pages/latihangit/Page18.jsx
@@ -3,7 +3,7 @@ export default function Page18() {
         <div className="p-4">
             <h1 className="text-x1 font-bold"> Halaman, Indi Marsela</h1>
             <div className="mt-4 space-x-4">
-                Halo selamat datang
+                Halo selamat datang mm
             </div>
         </div>
     );
