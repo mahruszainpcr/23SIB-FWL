6178e07d781783ce494f776b9532385222557fe6
Alya Verina <alya23sib@mahasiswa.pcr.ac.id>
2025-05-14 15:34:04 +0700

    Halaman Page6 sudah oke by alya

--- src/pertemuan_5/layouts/Sidebar.jsx
+++ src/pertemuan_5/layouts/Sidebar.jsx
@@ -120,6 +120,20 @@ export default function Sidebar() {
             </NavLink>
           </li>
 
+           <li>
+            <NavLink
+              to="/page6"
+              end
+              className={({ isActive }) =>
+                `${baseClass} ${isActive ? activeClass : ""}`
+              }
+              aria-label="Navigate to Page6"
+            >
+              <FaSignInAlt className="mr-4 text-xl" />
+              Page6
+            </NavLink>
+          </li>
+
           {/* Login */}
           <li>
             <NavLink
---
+++ src/pertemuan_5/pages/latihanagit/Page6.jsx
@@ -0,0 +1,10 @@
+export default function Page6() {
+    return(
+        <div className="p-4">
+            <h1 className="text-xl font-bold"> Halaman, Alya Verina</h1>
+            <div classname="mt-4 space-x-4" >
+                Halo Selamat Datang
+            </div>
+        </div>
+    );
+}
