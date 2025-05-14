5c7e130ace69935bc0845da7c1c814aa04e3b60a
Rafi <rafiqi23si@mahasiswa.pcr.ac.id>
2025-05-14 15:49:58 +0700

    Page01 sudah siap

---
+++ src/pages/latihangit/Page01.jsx
@@ -0,0 +1,10 @@
+import { DiVim } from "react-icons/di";
+
+export default function PageMZ(){
+    return(
+        <div className="p-4">
+            <h1 className="text-xl font-bold">Jo sok asik</h1>
+            <div className="mt-4 space-x-4" > Jangan sok asik jo nanti kena tangan</div>
+        </div>
+    )
+}
