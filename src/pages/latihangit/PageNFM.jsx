f4adba02fa39bec709eec95d1aa8f4ec4f1fb538
Muhammad Mahrus Zain <mahrus@pcr.ac.id>
2025-05-14 15:34:04 +0700

    Halaman Page NFM sudah oke by MMZ

---
+++ src/pages/latihangit/PageNFM.jsx
@@ -0,0 +1,10 @@
+export default function PageNFM() {
+    return(
+        <div className="p-4">
+            <h1 className="text-xl font-bold">halaman, fadil</h1>
+            <div className="mt-4 space-x-4">
+                    halo selamat datang
+            </div>
+        </div>   
+    )
+}
