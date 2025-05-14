2103f827a9459d4411c9861675189b70f26d2f46
Muhammad Alfayed <m.alfayed23si@mahasiswa.pcr.ac.id>
2025-05-14 15:34:32 +0700

    Halaman Page 25 sudah oke by MAL

---
+++ src/pages/latihangit/Page25.jsx
@@ -0,0 +1,10 @@
+export default function Page25() {
+    return(
+        <div className="p-4">
+            <h1 className="text-xl font-bold">Halaman, fayed</h1>
+            <div className="mt-4 space-x-4">
+                halo selamat datang
+            </div>
+        </div>
+    )
+}
--- src/pertemuan_5/main.jsx
+++ src/pertemuan_5/main.jsx
@@ -13,6 +13,7 @@ import Error from './pages/Error';
 import Login from './pages/Login';
 import Error401 from './pages/Error401';
 import MainLayout from './layouts/MainLayout';
+import Page25 from '../pages/latihangit/Page25';
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                                 <Route path="Invoice" element={<OrderInvoice />} />
                                 <Route path="customer" element={<Customer />} />
                                 <Route path="Settings" element={<CustomerSettings />} />
+                                <Route path="page25" element={<Page25 />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path="*" element={<Error/>} />
