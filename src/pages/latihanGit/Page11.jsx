3d0218b727416b9bf0206deb0ca766071ffc7452
Fadlan Alhamdi <fadlan23si@mahasiswa.pcr.ac.id>
2025-05-14 15:34:24 +0700

    Halaman Page 11 sudah oke by FA

---
+++ src/pages/latihanGit/Page11.jsx
@@ -0,0 +1,10 @@
+export default function Page11() {
+    return (
+        <div className="p-4">
+            <h1 className="text-xl font-bold ">Halaman, Fadlan Alhamdi</h1>
+            <div className="mt-4 space-x-4 ">
+                Halo Selamat datang
+            </div>
+        </div>
+    );
+}
--- src/pertemuan_5/main.jsx
+++ src/pertemuan_5/main.jsx
@@ -13,6 +13,7 @@ import Error from './pages/Error';
 import Login from './pages/Login';
 import Error401 from './pages/Error401';
 import MainLayout from './layouts/MainLayout';
+import Page11 from '../pages/latihanGit/Page11';
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                                 <Route path="Invoice" element={<OrderInvoice />} />
                                 <Route path="customer" element={<Customer />} />
                                 <Route path="Settings" element={<CustomerSettings />} />
+                                <Route path="Page11" element={<Page11 />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path="*" element={<Error/>} />
