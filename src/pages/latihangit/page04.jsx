af1e33391d5a5fc1354f49ebf68641833855cdf1
Aldi wiranto <Aldisama789@gmail.com>
2025-05-14 15:33:59 +0700

    p, Aldi

---
+++ src/pages/latihangit/page04.jsx
@@ -0,0 +1,8 @@
+export default function Page04() {
+    return(
+        <div className="p-4">
+            <h1 className="text-xl font-bold"> halaman, Aldi</h1>
+            <div className="mt-4 space-x-4"> halo selamat datang</div>
+        </div>
+    )
+}
--- src/pertemuan_5/main.jsx
+++ src/pertemuan_5/main.jsx
@@ -13,6 +13,8 @@ import Error from './pages/Error';
 import Login from './pages/Login';
 import Error401 from './pages/Error401';
 import MainLayout from './layouts/MainLayout';
+import Page04 from '../pages/latihangit/page04.jsx';
+
 
 const App = () => {
     return (
@@ -24,6 +26,7 @@ const App = () => {
                                 <Route path="Invoice" element={<OrderInvoice />} />
                                 <Route path="customer" element={<Customer />} />
                                 <Route path="Settings" element={<CustomerSettings />} />
+                                <Route path="page04" element={<Page04></Page04>} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path="*" element={<Error/>} />
