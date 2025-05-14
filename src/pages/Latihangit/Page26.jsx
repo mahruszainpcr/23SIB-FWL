0fffeb5b48eed8bc06076a9597061234d529c6d8
M.Hady Tiyo <m.hady23ssi@mahasiswa.pcr.ac.id>
2025-05-14 15:34:51 +0700

    Halaman Page26 sudah oke by hd

---
+++ src/pages/Latihangit/Page26.jsx
@@ -0,0 +1,10 @@
+export default function Page26(){ 
+    return ( 
+        <div className="p-4"> 
+            <h1 className="text-xl font-bold ">Halaman, M.hady Tiyo</h1> 
+            <div className="mt-4 space-x-4 "> 
+                Halo selamat Datang 
+            </div> 
+        </div>  
+    );
+}
--- src/pertemuan_5/main.jsx
+++ src/pertemuan_5/main.jsx
@@ -13,6 +13,7 @@ import Error from './pages/Error';
 import Login from './pages/Login';
 import Error401 from './pages/Error401';
 import MainLayout from './layouts/MainLayout';
+import Page26 from '../pages/Latihangit/Page26.jsx';
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                                 <Route path="Invoice" element={<OrderInvoice />} />
                                 <Route path="customer" element={<Customer />} />
                                 <Route path="Settings" element={<CustomerSettings />} />
+                                <Route path="Page26" element={<Page26 />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path="*" element={<Error/>} />
