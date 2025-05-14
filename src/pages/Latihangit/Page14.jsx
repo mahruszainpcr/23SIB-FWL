93dc524ebf3c3de7e319c519b7b1259175027461
fathir23si@mahasiswa.pcr.ac.id <fathir23si@mahasiswa.pcr.ac.id>
2025-05-14 15:52:06 +0700

    ini halaman page 14 sudah oke by fathir

---
+++ src/pages/Latihangit/Page14.jsx
@@ -0,0 +1,10 @@
+export default function Page14(){
+    return (
+        <divc className="p-4">
+            <h1 className="text-x1 font-bold ">Haloo, Fathir Al Qadri</h1>
+            <div className="mt-4 space-x-4">
+                Welcome BOS
+            </div>
+        </divc>
+    )
+}
